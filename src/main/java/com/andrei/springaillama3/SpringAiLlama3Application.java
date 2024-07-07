e546a2adad58a401ce21f1a80453c16b79bc73a4
Andrei <andreibalzan@gmail.com>
2024-07-07 18:00:37 +0100

    Initial commit

---
+++ src/main/java/com/andrei/springaillama3/SpringAiLlama3Application.java
@@ -0,0 +1,13 @@
+package com.andrei.springaillama3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiLlama3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAiLlama3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/andrei/springaillama3/SpringAiLlama3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andrei.springaillama3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiLlama3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
